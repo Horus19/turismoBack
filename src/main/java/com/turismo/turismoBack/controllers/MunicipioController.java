7c079b35feae7ac8c51d1712c60f3cc19805230c
Felipe Rojas <andres2180021@correo.uis.edu.co>
2022-09-21 14:46:31 -0500

    cross all enable

--- src/main/java/com/turismo/turismoBack/controllers/MunicipioController.java
+++ src/main/java/com/turismo/turismoBack/controllers/MunicipioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin
 @RequestMapping("/api")
 public class MunicipioController {
 
